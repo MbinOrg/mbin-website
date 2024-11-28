df3785b3b98528678a955745f748dbf04c77c9d9
jwr1 <47087725+jwr1@users.noreply.github.com>
2024-11-27 22:07:16 -0500

    Fix markdown component not rendering anything

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,10 +1,13 @@
-import { clientOnly } from '@solidjs/start';
-import { ParentComponent } from 'solid-js';
+import remarkGfm from 'remark-gfm';
+import { Component } from 'solid-js';
+import { SolidMarkdown } from 'solid-markdown';
 
-const ClientOnlyComp = clientOnly(() => import('./MarkdownInner'));
-
-const Markdown: ParentComponent = (props) => {
-  return <ClientOnlyComp>{props.children}</ClientOnlyComp>;
+const Markdown: Component<{ children: string }> = (props) => {
+  return (
+    <SolidMarkdown class="text-left markdown" remarkPlugins={[remarkGfm]}>
+      {props.children}
+    </SolidMarkdown>
+  );
 };
 
 export default Markdown;

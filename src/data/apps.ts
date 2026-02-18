5b38d7cb049ff846965c98c9374c66ab229c3451
jwr1 <47087725+jwr1@users.noreply.github.com>
2026-02-18 14:57:49 -0500

    Update Interstellar data (#9)

--- src/data/apps.ts
+++ src/data/apps.ts
@@ -18,12 +18,13 @@ export interface App {
 export const apps: App[] = [
   {
     name: 'Interstellar',
-    icon: 'https://raw.githubusercontent.com/jwr1/interstellar/6d8fce0972febccec91fa056746fdb9f4f960217/assets/icons/logo.png',
+    icon: 'https://raw.githubusercontent.com/interstellar-app/interstellar/77289c13eb63347ad68aca20016c2daa35855f7a/assets/icons/logo.png',
     description: 'An app for Mbin and Lemmy, connecting you to the fediverse.',
     links: {
+      appleStore: 'https://apps.apple.com/us/app/id6745356296',
       fdroid: 'https://apt.izzysoft.de/fdroid/index/apk/one.jwr.interstellar',
       flathub: 'https://flathub.org/apps/one.jwr.interstellar',
-      github: 'https://github.com/jwr1/interstellar',
+      github: 'https://github.com/interstellar-app/interstellar',
       googlePlay:
         'https://play.google.com/store/apps/details?id=one.jwr.interstellar',
       matrix: 'https://matrix.to/#/#interstellar-space:matrix.org',
